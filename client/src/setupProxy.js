6e490a3e6f0802353cb7a5174b4095b4fc22e3e0
Denis Stryzhalov <imperor.oktov@gmail.com>
2024-01-25 19:53:45 +0200

    fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api/', // the endpoint you want to proxy
     createProxyMiddleware({
-      target: 'https://43x2uexmfx.eu-central-1.awsapprunner.com:8080', // the address of the server you want to proxy to
+      target: 'http://43x2uexmfx.eu-central-1.awsapprunner.com:8080', // the address of the server you want to proxy to
       changeOrigin: true,
     })
   );
